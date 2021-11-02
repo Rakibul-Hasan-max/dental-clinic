f7496693d8330cea4ca58762b9feaf5013c6328e
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-02 20:13:43 +0600

    Dental-Clinic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            {/* <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route> */}
+            </Switch>
+        </Router>
     </div>
   );
 }
